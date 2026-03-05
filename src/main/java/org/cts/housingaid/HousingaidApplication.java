897f6f3c1f1f62b423ac961d4812ff8e221dc895
PawanK8627 <Pawan.Kumar3@cognizant>
2026-03-05 13:45:37 +0530

    Project File Structure

---
+++ src/main/java/org/cts/housingaid/HousingaidApplication.java
@@ -0,0 +1,18 @@
+package org.cts.housingaid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousingaidApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(HousingaidApplication.class, args);
+
+
+
+
+    }
+
+}
---
+++ src/test/java/org/cts/housingaid/HousingaidApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cts.housingaid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousingaidApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
