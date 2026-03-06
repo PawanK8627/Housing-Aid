073188e9cb7727564926d1f2aa028b4007e43f06
Logesh <2478081@cognizant.com>
2026-03-06 14:05:36 +0530

    Committed

--- src/main/java/org/cts/housingaid/entity/HousingUnit.java
+++ src/main/java/org/cts/housingaid/entity/HousingUnit.java
@@ -7,4 +7,43 @@ public class HousingUnit {
     private int capacity;
     private String status;
 
+    public int getUnitId() {
+        return unitId;
+    }
+
+    public void setUnitId(int unitId) {
+        this.unitId = unitId;
+    }
+
+    public String getLocation() {
+        return Location;
+    }
+
+    public void setLocation(String location) {
+        Location = location;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
